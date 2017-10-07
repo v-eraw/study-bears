9a49f21cc5ff25e36e415d2afe9e4a4ba66d6319
katie-gu <katiegu555@gmail.com>
2017-10-07 14:16:02 -0700

    Finished homepage.

---
+++ StudyBears/app/src/main/java/com/example/katiegu/studybears/Home.java
@@ -0,0 +1,32 @@
+package com.example.katiegu.studybears;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.*;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        LinearLayout root = (LinearLayout) findViewById(R.id.rootLayout);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT, 0.0F));
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        TextView dynamicTextView = new TextView(this);
+        dynamicTextView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        dynamicTextView.setText("Welcome to StudyBears!");
+
+        layout.addView(dynamicTextView);
+        root.addView(layout);
+
+        setContentView(root);
+    }
+}
