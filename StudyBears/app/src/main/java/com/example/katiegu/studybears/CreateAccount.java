2a710895368c4068f9deb28f8f4d86a89ef9a8be
katie-gu <katiegu555@gmail.com>
2017-10-07 22:53:48 -0700

    Working on sign up.

--- StudyBears/app/src/main/java/com/example/katiegu/studybears/CreateAccount.java
+++ StudyBears/app/src/main/java/com/example/katiegu/studybears/CreateAccount.java
@@ -1,41 +1,42 @@
 package com.example.katiegu.studybears;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class LogIn2 extends AppCompatActivity {
-
-    private static final String TAG = "LogIn2";
+public class CreateAccount extends AppCompatActivity {
+    private static final String TAG = "CreateAccount";
+    private FirebaseDatabase mFireBaseDatabase;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private String userID;
 
-    //UI References
-    private EditText mEmail, mPassword;
-    private Button btnSignIn, btnCreateAccount;
-
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in2);
+        setContentView(R.layout.activity_create_account);
 
-        //Declare buttons and edit texts.
-        mEmail = (EditText) findViewById(R.id.email);
-        mPassword = (EditText) findViewById(R.id.password);
-        btnSignIn = (Button) findViewById(R.id.email_sign_in_button);
-        btnCreateAccount = (Button) findViewById(R.id.create_account_button);
+        mListView = (ListView) findViewById(R.id.listview);
 
         mAuth = FirebaseAuth.getInstance();
+        mFireBaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFireBaseDatabase.getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+        userID = user.getUid();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -50,28 +51,26 @@ public class LogIn2 extends AppCompatActivity {
             }
         };
 
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                String email = mEmail.getText().toString();
-                String pass = mPassword.getText().toString();
-                if (!email.equals("") && !pass.equals("")) {
-                    mAuth.signInWithEmailAndPassword(email, pass);
-                } else {
-                    toastMessage("You didn't fill in all the fields.");
-                }
-            }
-        });
-
-        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LogIn2.this, CreateAccount.class);
-                startActivity(intent);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                showData(dataSnapshot);
             }
 
-});
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        }
+
+        private void showData(DataSnapshot dataSnapshot) {
+            for (DataSnapshot ds: dataSnapshot.getChildren()) {
+                UserInformation uInfo = new UserInformation();
+
+            }
+        }
     }
+
     @Override
     public void onStart() {
         super.onStart();
@@ -94,4 +93,5 @@ public class LogIn2 extends AppCompatActivity {
     private void toastMessage(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
 }
---
+++ StudyBears/app/src/main/java/com/example/katiegu/studybears/UserInformation.java
@@ -0,0 +1,12 @@
+package com.example.katiegu.studybears;
+
+/**
+ * Created by katiegu on 10/7/17.
+ */
+
+public class UserInformation {
+    private String name;
+    private String email;
+    private String school;
+    private String
+}
